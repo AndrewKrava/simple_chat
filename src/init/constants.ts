5eebaadd4a56f9a6d34227355373f4df6f8a3e04
AndrewKrava <kravecandrew@gmail.com>
2022-04-24 14:37:27 +0300

    save

--- src/init/constants.ts
+++ src/init/constants.ts
@@ -2,8 +2,8 @@
 export const API_URL = process.env.API_URL || 'http://localhost:4000';
 export const ROOT_URL = 'https://api.barbarossa.pp.ua/';
 export const REFRESH_AUTH_PATH = ROOT_URL + 'users/refresh/';
-// export const REGISTRATION_PATH = ROOT_URL + 'users/register';
-export const REGISTRATION_PATH = ROOT_URL + 'users1111/register';   // with error
+export const REGISTRATION_PATH = ROOT_URL + 'users/register';
+// export const REGISTRATION_PATH = ROOT_URL + 'users1111/register';   // with error
 
 
 // Local
