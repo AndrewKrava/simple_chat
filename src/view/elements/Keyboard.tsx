4bc7a1d5ace45b20304b5e2ae5dbbf4ab9035a46
unknown <kravecandrew@gmail.com>
2022-05-02 11:25:50 +0300

    save

--- src/view/elements/Keyboard.tsx
+++ src/view/elements/Keyboard.tsx
@@ -1,5 +1,5 @@
 // Core
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 
@@ -13,25 +13,49 @@ type PropsType = {
 }
 
 export const Keyboard: FC<PropsType> = (props) => {
+    const el = useRef<HTMLInputElement | null>(null);
+    const listener = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        // el.current?.addEventListener('keypress', () => {
+        //     console.log('event key press');
+        // });
+        window.addEventListener('keypress', () => {
+            console.log('event key press');
+        });
+    }, []);
+
+
     const handler = (event: React.KeyboardEvent<HTMLInputElement>) => {
         console.log('event: ', event);
     };
 
     const eventPress = () => {
         // document.dispatchEvent
+        // console.log('ref ', el);
+        el.current?.dispatchEvent(new KeyboardEvent('keypress', { key: 'a', which: 65 }));
     };
+
+    console.log('el ', el);
+    console.log('el current ', el.current);
+
 
     return (
         <Container>
             <input
+                ref = { el }
                 type = 'text'
                 // onChange = { event =>  }
                 // onKeyDown = { (event) => handler(event) }
                 // onKeyPress = { (event) => handler(event) }
                 // onKeyUp = { (event) => handler(event) }
             />
-            <div>
-                <button onClick = { () => eventPress() }>press</button>
+
+            <div ref = { listener }>listener
+                <div>
+                    <button onClick = { () => eventPress() }>press</button>
+                </div>
+
             </div>
         </Container>
     );
